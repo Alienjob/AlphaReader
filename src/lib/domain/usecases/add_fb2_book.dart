2a4f90d3d662f7c3f573d82d83e318bf351e806e
Alien_job <Alien_job@list.ru>
2022-10-09 11:28:39 +0300

    optimization transliteration

--- src/lib/domain/usecases/add_fb2_book.dart
+++ src/lib/domain/usecases/add_fb2_book.dart
@@ -1,6 +1,5 @@
 import 'package:AlphaReader/domain/entities/book.dart';
 import 'package:AlphaReader/domain/repositories/books_repository.dart';
-import 'package:AlphaReader/domain/usecases/choose_book.dart';
 import 'package:AlphaReader/features/book_list/data/fb2/fb2_book.dart';
 import 'package:AlphaReader/features/core/data/user_data_repository.dart';
 
--- src/lib/mixer/mixer.dart
+++ src/lib/mixer/mixer.dart
@@ -17,28 +17,9 @@ class Mixer {
   }
 
   String mix(String text) {
-    String result = '';
-    for (int i = 0; i < text.length - 1; i++) {
-      String key2 = text.substring(i, i + 2).toLowerCase();
-      String key1 = text.substring(i, i + 1).toLowerCase();
-
-      if (subMap[key2] != null) {
-        result = addSub(result, subMap[key2]);
-        i++;
-      } else if (subMap[key1] != null) {
-        result = addSub(result, subMap[key1]);
-      } else {
-        result = result + key1;
-      }
-    }
-
-    String key1 = text.substring(text.length - 1, text.length);
-
-    if (subMap[key1] != null) {
-      result = addSub(result, subMap[key1]);
-    } else {
-      result = result + key1;
-    }
+    String pattern = '(${[...subMap.keys].join('|')})';
+    String result = text.toLowerCase().replaceAllMapped(
+        RegExp(pattern), (match) => _transliterate(match, subMap));
     return result;
   }
 
@@ -47,5 +28,12 @@ class Mixer {
       return text + subs[0];
     }
     return text;
+  }
+
+  String _transliterate(match, subMap) {
+    String from = match?.group(0);
+    List<String> to = subMap[from] ?? [];
+    String result = (to.isNotEmpty) ? to[0] : from;
+    return result;
   }
 }
