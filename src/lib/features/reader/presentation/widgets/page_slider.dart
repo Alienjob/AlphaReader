f8684cdb1cef0b5bc03ea39ad1f2a6f8abee4949
Alien_job <Alien_job@list.ru>
2023-04-30 21:40:20 +0400

    refactor

--- src/lib/features/reader/presentation/widgets/page_slider.dart
+++ src/lib/features/reader/presentation/widgets/page_slider.dart
@@ -44,58 +44,9 @@ class _PageSliderState extends State<PageSlider> {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            IconButton(
-              icon: Icon(
-                Icons.font_download,
-                color: Theme.of(context).cardColor,
-              ),
-              onPressed: () async {
-                //  sl<ReaderBloc>().add(ReaderEventNextFont());
-                var selectedFont = await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    //builder: (context) => const ShopPage(),
-                    builder: (context) => const FontsPage(),
-                  ),
-                );
-                sl<ReaderBloc>().add(ReaderEventSelectFont(selectedFont));
-              },
-            ),
-            Row(
-              children: [
-                IconButton(
-                    onPressed: () {
-                      sl<ReaderBloc>().add(ReaderEventDecreaseFontSize());
-                    },
-                    icon: Icon(
-                      Icons.arrow_back_ios,
-                      color: Theme.of(context).cardColor,
-                    )),
-                Icon(
-                  Icons.format_size,
-                  color: Theme.of(context).cardColor,
-                ),
-                IconButton(
-                    onPressed: () {
-                      sl<ReaderBloc>().add(ReaderEventIncreaseFontSize());
-                    },
-                    icon: Icon(
-                      Icons.arrow_forward_ios,
-                      color: Theme.of(context).cardColor,
-                    ))
-              ],
-            ),
-            TextButton(
-              child: Text(
-                setLabel,
-                style: TextStyle(
-                  color: Theme.of(context).cardColor,
-                ),
-              ),
-              onPressed: () {
-                sl<ReaderBloc>().add(ReaderEventNextSet());
-              },
-            ),
+            FontSelector(),
+            FontSizeSelector(),
+            LanguageSelector(setLabel: setLabel),
           ],
         ),
         Divider(color: Theme.of(context).cardColor),
@@ -129,6 +80,91 @@ class _PageSliderState extends State<PageSlider> {
           ),
         )
       ],
+    );
+  }
+}
+
+class LanguageSelector extends StatelessWidget {
+  const LanguageSelector({
+    super.key,
+    required this.setLabel,
+  });
+
+  final String setLabel;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      child: Text(
+        setLabel,
+        style: TextStyle(
+          color: Theme.of(context).cardColor,
+        ),
+      ),
+      onPressed: () {
+        sl<ReaderBloc>().add(ReaderEventNextSet());
+      },
+    );
+  }
+}
+
+class FontSizeSelector extends StatelessWidget {
+  const FontSizeSelector({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+            onPressed: () {
+              sl<ReaderBloc>().add(ReaderEventDecreaseFontSize());
+            },
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Theme.of(context).cardColor,
+            )),
+        Icon(
+          Icons.format_size,
+          color: Theme.of(context).cardColor,
+        ),
+        IconButton(
+            onPressed: () {
+              sl<ReaderBloc>().add(ReaderEventIncreaseFontSize());
+            },
+            icon: Icon(
+              Icons.arrow_forward_ios,
+              color: Theme.of(context).cardColor,
+            ))
+      ],
+    );
+  }
+}
+
+class FontSelector extends StatelessWidget {
+  const FontSelector({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        Icons.font_download,
+        color: Theme.of(context).cardColor,
+      ),
+      onPressed: () async {
+        //  sl<ReaderBloc>().add(ReaderEventNextFont());
+        var selectedFont = await Navigator.push(
+          context,
+          MaterialPageRoute(
+            //builder: (context) => const ShopPage(),
+            builder: (context) => const FontsPage(),
+          ),
+        );
+        sl<ReaderBloc>().add(ReaderEventSelectFont(selectedFont));
+      },
     );
   }
 }
