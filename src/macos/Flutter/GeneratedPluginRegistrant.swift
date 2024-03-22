5b94d84f70cb1090a13d366330da754a5085b483
Igor Pashutin <igor.pashutin.georgia@gmail.com>
2024-03-22 05:23:11 +0400

    build artifacts

--- src/macos/Flutter/GeneratedPluginRegistrant.swift
+++ src/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,22 +5,14 @@
 import FlutterMacOS
 import Foundation
 
-import audio_session
-import just_audio
 import path_provider_foundation
 import purchases_flutter
 import shared_preferences_foundation
-import sqflite
-import url_launcher_macos
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
-  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
