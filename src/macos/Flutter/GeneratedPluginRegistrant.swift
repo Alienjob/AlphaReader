c1fca583b266db04842db7b54695e4653ab76e06
Igor Pashutin <igor.pashutin.georgia@gmail.com>
2024-09-28 14:48:17 +0400

    update sdk for support google standarts

--- src/macos/Flutter/GeneratedPluginRegistrant.swift
+++ src/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,26 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
+import just_audio
+import package_info_plus
 import path_provider_foundation
 import purchases_flutter
 import shared_preferences_foundation
-import wakelock_macos
+import sqflite
+import url_launcher_macos
+import video_player_avfoundation
+import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
